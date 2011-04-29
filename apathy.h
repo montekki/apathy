82a2eefac10afd1c189a0c9946f9cd0a7ac34ac6
Fedor Sakharov <sakharov@lvk.cs.msu.su>
2011-04-29 07:48:15 +0000

    apathy: Adds comments to apathy.h.

--- apathy.h
+++ apathy.h
@@ -9,23 +9,31 @@
 #include <sys/mman.h>
 #endif
 
-/*FIXME: Check ioctl number */
 
-/** max assumed length of SELinux context */
-
+/* max assumed length of SELinux context */
 #define CONT_MAXLEN 		256
 
+/*FIXME: Check ioctl number */
 #define APATHY_IOCTL_MAGIC 	0x8e
 
+/*! \struct apathy_trans
+  \brief information about context transition point
+  */
 struct apathy_trans {
 	unsigned int pid; 		/*!< pid of the process */
 	unsigned long addr; 		/*!< address of the break */
 	char new_cont[CONT_MAXLEN]; 	/*!< new context */
 };
 	
+/*! \def APATHY_IOCTL_SET_BREAK
+  \brief set a new context transition control point
+  */
 #define APATHY_IOCTL_SET_BREAK \
 	_IOWR(APATHY_IOCTL_MAGIC, 0, struct apathy_trans)
 
+/*! \def APATHY_IOCTL_DEL_BREAK \
+  \brief delete context transition control point
+  */
 #define APATHY_IOCTL_DEL_BREAK \
 	_IOWR(APATHY_IOCTL_MAGIC, 1, struct apathy_trans)
 
